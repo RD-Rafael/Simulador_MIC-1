794c37c13a8e5659a008411dc4678816cf3a6f29
Rafael Rocha <efp.rafael.rd@gmail.com>
2026-04-21 17:03:19 -0300

    add text input

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ import globals
 import pygame
 import pygame_textinput
 
-
 running = True
 
 textInput = pygame_textinput.TextInputVisualizer(font_object=globals.default_font)
+pygame.key.set_repeat(400, 25)
 while running:
 
-    for event in pygame.event.get():
+    events = pygame.event.get()
+
+
+    textInput.update(events)
+
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
 
-    events = pygame.event.get()
-    textInput.update(events)
-    
-    
     globals.screen.fill("blue")
     
     globals.screen.blit(textInput.surface, (10,10))
-    globals.screen.blit(globals.default_font.render("Hello world", True, (255,0,0)), (0,0))
+    globals.screen.blit(globals.default_font.render("Hello World", True, (255,0,0)), (0,0))
 
     pygame.display.flip()
 
